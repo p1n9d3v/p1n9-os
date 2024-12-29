88ff9a598e8e66f8af7ec7ae2b598a89eb28d43f
p1n9d3v <p1n9@p1n9s-MacBook-Air.local>
2024-12-30 00:31:53 +0900

    test: Dynamic Import 테스트

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -1,10 +1,11 @@
-import { expect, test } from 'vitest'
+import { vi, expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Page from '@/app/page'
 
-test('Page', () => {
+test('Page', async () => {
     render(<Page />)
+    await vi.dynamicImportSettled()
     expect(
-        screen.getByRole('heading', { level: 1, name: 'Hello' })
+        screen.getByRole('heading', { level: 1, name: 'Hello Zero' })
     ).toBeDefined()
 })
