639d7d3a38c10c21c44dc3d80ecd5224956c9d89
p1n9d3v <p1n9@p1n9s-MacBook-Air.local>
2024-12-31 18:33:37 +0900

    chore: react strict mode 활성화

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
 import type { NextConfig } from 'next'
 
 const withVanillaExtract = createVanillaExtractPlugin()
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+    reactStrictMode: true,
+}
 
 export default withVanillaExtract(nextConfig)
